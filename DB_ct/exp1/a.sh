efd0a3ea9496c0457dc966760de6819f260d64d9
tongcheng <759344324@qq.com>
2019-10-23 22:46:15 +0800

    Delete DB_ct

--- DB_ct/exp1/a.sh
+++
@@ -1,4 +0,0 @@
-cp source.c source.pgc
-../ecpg -t -c -I ../include -o ./intermediate.c source.pgc 
-gcc -I ../include -Wall -g ./intermediate.c -L ../lib -lecpg -lpq -lpgtypes -o ./execute
-./execute
